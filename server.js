b71598ec8c9753ae250c40dcc05f447ba9f51454
Oleh Lavryk <oleh.lavrik@gmail.com>
2021-03-09 20:52:04 +0200

    Revert changes

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("./database/routes.json");
+const router = jsonServer.router("./database/index.json");
 const middlewares = jsonServer.defaults({
   static: "./build",
 });
